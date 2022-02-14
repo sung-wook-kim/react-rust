ae201893b120862d0e610c502161a08cc16e151d
sung-wook-kim <plackyou123@korea.ac.kr>
2022-02-14 12:26:45 +0900

    Updates

---
+++ precache-manifest.c4a7e184a506dba585fb841ac6209fd9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a4c731af5a9c337869142ebfe899e7",
+    "url": "/react-rust/index.html"
+  },
+  {
+    "revision": "ca23c7128d39bd685493",
+    "url": "/react-rust/static/js/2.00eb27c5.chunk.js"
+  },
+  {
+    "revision": "639d85c13a823a47dab5",
+    "url": "/react-rust/static/js/main.aa68cd72.chunk.js"
+  },
+  {
+    "revision": "c28aaed5659aa4daecd3",
+    "url": "/react-rust/static/js/runtime~main.1a9ddea2.js"
+  }
+]);
